5613fb6abc394a0cef5d200f4f411509da041039
Ryan O'Connor <ryan@assemblyai.com>
2024-11-21 15:41:57 -0500

    initial

---
+++ utils/__init__.py
@@ -0,0 +1,38 @@
+import os
+import shutil
+
+import ffmpeg
+
+
+def combine_tracks(filepath="combined_audio.m4a", dir="tmp"):
+    # List input files from the 'tmp' directory
+    input_files = [os.path.join(dir, file) for file in os.listdir(dir) if file.endswith('.m4a')]
+    if not input_files:
+        raise ValueError("No input files found in the 'tmp' directory.")
+
+    # Create FFmpeg input streams
+    inputs = [ffmpeg.input(file) for file in input_files]
+
+    # Number of input streams
+    num_inputs = len(input_files)
+
+    # Construct the filter_complex for the 'join' filter
+    # Use a custom channel layout: '<num_inputs>.0'
+    if len(input_files) ==1:
+        shutil.copy(input_files[0], filepath)
+        return
+    elif len(input_files) == 2:
+        join_filter = f'join=inputs={num_inputs}'
+    else:
+        # ffmpeg incorrectly assumes 2 channel => stereo rather than dual mono in case of two files
+        join_filter = f'join=inputs={num_inputs}:channel_layout={num_inputs}.0'
+        
+
+    # Run the ffmpeg command to combine the tracks
+    try:
+        ffmpeg.output(*inputs, filepath, filter_complex=join_filter, **{'loglevel': 'error'}).run()
+    except ffmpeg.Error as e:
+        # Check if stderr is None or empty and raise more detailed error
+        error_message = e.stderr.decode() if e.stderr else f"No detailed error message available. Raw error: {str(e)}"
+        print(f"FFmpeg Error: {error_message}")  # Print the error message to help with debugging
+        raise RuntimeError(f"FFmpeg error: {error_message}") from e
