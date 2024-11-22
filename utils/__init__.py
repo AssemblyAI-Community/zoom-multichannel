5b2d9c5a5aeb15f4103b144f24a89f878e9fc8d8
Ryan O'Connor <46252299+oconnoob@users.noreply.github.com>
2024-11-22 12:09:58 -0500

    Update filter for cross-compatibility

--- utils/__init__.py
+++ utils/__init__.py
@@ -13,20 +13,22 @@ def combine_tracks(filepath="combined_audio.m4a", dir="tmp"):
     inputs = [ffmpeg.input(file) for file in input_files]
     num_inputs = len(input_files)
 
-    if len(input_files) ==1:
+    if len(input_files) == 1:
         shutil.copy(input_files[0], filepath)
         return
     elif len(input_files) == 2:
-        join_filter = f'join=inputs={num_inputs}'
+        join_filter = f'[0:a][1:a]join=inputs={num_inputs}[a]'
     else:
         # ffmpeg incorrectly assumes 2 channel => stereo rather than dual mono in case of two files
-        join_filter = f'join=inputs={num_inputs}:channel_layout={num_inputs}.0'
-        
+        join_filter = f'[0:a][1:a]join=inputs={num_inputs}:channel_layout={num_inputs}.0[a]'
 
     # Run the ffmpeg command to combine the tracks
     try:
-        ffmpeg.output(*inputs, filepath, filter_complex=join_filter, **{'loglevel': 'error'}).run()
+        a = ffmpeg.output(*inputs, filepath, filter_complex=join_filter, **{'loglevel': 'error', 'map': '[a]'}).compile()
+        print(a)
+        return a
     except ffmpeg.Error as e:
         # Check if stderr is None or empty and raise more detailed error
         error_message = e.stderr.decode() if e.stderr else f"No detailed error message available. Raw error: {str(e)}"
+        print(f"FFmpeg Error: {error_message}")  # Print the error message to help with debugging
         raise RuntimeError(f"FFmpeg error: {error_message}") from e
